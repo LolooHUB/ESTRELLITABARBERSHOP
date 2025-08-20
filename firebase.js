74ca84bf239737056128c0f091c791f949c0c5ac
LolooHUB <lololicastro25@gmail.com>
2025-08-20 19:45:29 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "XXXXXXXX",
-  appId: "XXXXXXXX"
+  apiKey: "AIzaSyDX7koFnRGzFPjH_InoyzqmIIKU-ZuCL8U",
+  authDomain: "estrellitabarbershop-290ed.firebaseapp.com",
+  projectId: "estrellitabarbershop-290ed",
+  storageBucket: "estrellitabarbershop-290ed.firebasestorage.app",
+  messagingSenderId: "610086743105",
+  appId: "1:610086743105:web:fcbc532b67926c3e36aac0",
+  measurementId: "G-TEM96CH6DT"
 };
 
 const app = initializeApp(firebaseConfig);
