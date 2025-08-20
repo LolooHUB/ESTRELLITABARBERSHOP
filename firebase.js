44212ed79243f51240556f44b540b4a8f2d5a925
LolooHUB <lololicastro25@gmail.com>
2025-08-20 19:42:18 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// IMPORTANTE: Reemplazá con tu configuración de Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "XXXXXXXX",
+  appId: "XXXXXXXX"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
